0cd7f68645755598367acecb50f551e47786159a
Daniel <ETHZ>
2016-10-18 22:24:25 +0200

    Back to /sbin/hello

--- usr/init/main.c
+++ usr/init/main.c
@@ -79,16 +79,16 @@ int main(int argc, char *argv[])
         DEBUG_ERR(err, "slot_alloc_init");
     }
 
-//    struct spawninfo* process_info = malloc(sizeof(struct spawninfo));
-//    err = spawn_load_by_name("/armv7/sbin/init", process_info);
-//    if(err_is_fail(err)){
-//        DEBUG_ERR(err, "spawn_load_by_name");
-//    }
-//    free(process_info);
+    struct spawninfo* process_info = malloc(sizeof(struct spawninfo));
+    err = spawn_load_by_name("/armv7/sbin/hello", process_info);
+    if(err_is_fail(err)){
+        DEBUG_ERR(err, "spawn_load_by_name");
+    }
+    free(process_info);
 
     debug_printf("Runing tests!\n");
 
-    test_paging();
+//    test_paging();
 //    runtests_mem_alloc();
 
     debug_printf("Message handler loop\n");
