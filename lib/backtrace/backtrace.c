d545fd5391dce4eb42dac40814b4e03d84ef5f4a
Daniel <ETHZ>
2016-11-24 22:41:47 +0100

    Fixed crash in backtrace

--- lib/backtrace/backtrace.c
+++ lib/backtrace/backtrace.c
@@ -12,7 +12,7 @@ void backtrace_from_fp(int topfp)
 {
     const char* args = "addr2line -f -e armv7/sbin/init";
     debug_printf("Backtrace from fp @ 0x%08x\n", topfp);
-    for (int i=0; i < 10; i++) {
+    for (int i=0; i < 10 && topfp; i++) {
         int fp = *(((int*)topfp) -3);
         //int sp = *(((int*)topfp) -2);
         int lr = *(((int*)topfp) -1);
@@ -25,5 +25,4 @@ void backtrace_from_fp(int topfp)
             printf("%s 0x%08x\n", args, pc);
         topfp = fp;
     }
-    while(true);
 }
