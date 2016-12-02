2b172ebf2994310eaf224a7f036a1319d0a0c041
Daniel <ETHZ>
2016-12-02 15:43:08 +0100

    Shell: 'ps' command

--- usr/shell/commands_handlers.c
+++ usr/shell/commands_handlers.c
@@ -1,3 +1,4 @@
+#include <aos/aos_rpc.h>
 #include "shell.h"
 
 /**
@@ -7,7 +8,7 @@ Commands:
     - threads
     - memtest
     - oncore
-    - ps
+    - [OK] ps
     - [OK] help
 */
 
@@ -33,6 +34,30 @@ static void handle_args(char* const argv[], int argc)
         debug_printf("arg[%d]: '%s'\n", i, argv[i]);
 }
 
+static void handle_ps(char* const argv[], int argc)
+{
+    domainid_t* pidptr;
+    uint32_t pidcount;
+	errval_t err = aos_rpc_process_get_all_pids(get_init_rpc(), &pidptr, &pidcount);
+	if (err_is_fail(err)){
+		DEBUG_ERR(err, "Could not get PIDs");
+		return;
+	}
+    printf("%u running processes:\n", pidcount);
+	char* name;
+	for (int i = 0; i < pidcount; i++) {
+		err = aos_rpc_process_get_name(get_init_rpc(), pidptr[i], &name);
+		if (err_is_ok(err))
+		{
+			printf("\t%d\t\"%s\"\n", pidptr[i], name);
+			free(name);
+		}
+		else
+			DEBUG_ERR(err, "Could not get domain name [pid=%d]\n", pidptr[i]);
+	}
+	free(pidptr);
+}
+
 
 
 bool shell_execute_command(char* const argv[], int argc)
@@ -53,6 +78,7 @@ struct command_handler_entry* shell_get_command_table(void)
         {.name = "echo",        .handler = handle_echo},
         {.name = "help",        .handler = handle_help},
         {.name = "args",        .handler = handle_args},
+        {.name = "ps",          .handler = handle_ps},
         {.name = NULL}
     };
     return commandsTable;
