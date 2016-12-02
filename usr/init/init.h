ef5cec44c628a62e83345e8fb82fc388016e7c34
Daniel <ETHZ>
2016-12-02 12:38:41 +0100

    We should be ok for some time with this...

--- usr/init/init.h
+++ usr/init/init.h
@@ -15,6 +15,6 @@ struct urpc_channel urpc_chan; // URPC thread holds reference to this
 errval_t os_core_initialize(int argc, char** argv);
 errval_t os_core_events_loop(void);
 
-#define NUM_BOOTINFO_REGIONS 8
+#define NUM_BOOTINFO_REGIONS 20
 
 #endif
