3a88baf57d35e007416ba27030237165dd81e97e
Daniel <ETHZ>
2016-12-03 13:33:01 +0100

    Shell: Finally handle quoted arguments, escapes etc.. correctly

--- usr/shell/io.c
+++ usr/shell/io.c
@@ -19,17 +19,49 @@ static bool advance_to_begin_of_arg(size_t* i, char* line, size_t end_pos)
         ++(*i);
     return *i < end_pos;
 }
+
+/*
+    \X... -> X...
+*/
+static void unescape_chars(size_t begin, size_t end, char* string)
+{
+    for (; begin < end-1; ++begin)
+    {
+        if (string[begin] == '\\')
+        {
+            if (string[begin+1] == 'n')
+                string[begin+1] = '\n';
+            if (string[begin+1] == 't')
+                string[begin+1] = '\t';
+            for (int i = begin; i < end - 1; ++i)
+                string[i] = string[i+1];
+            string[end - 1] = 0;
+            --end;
+        }
+    }
+}
+
 static void advance_to_end_of_arg(size_t* begin, size_t* i, char* line, size_t end_pos)
 {
     // Quoted args
     if (line[*i] == '"')
     {
         size_t quote_end = *i + 1;
-        while (line[quote_end] != '"' && quote_end < end_pos)
+        int escape_count = 0;
+        while (quote_end < end_pos)
+        {
+            if (line[quote_end] == '"' && (escape_count % 2) == 0)
+                break;
+            if (line[quote_end] == '\\')
+                ++escape_count;
+            else
+                escape_count = 0;
             ++quote_end;
+        }
         if (quote_end != end_pos) // We found end of quote
         {
             assert(line[quote_end] == '"');
+            unescape_chars(*i, quote_end, line);
             *i = quote_end;
             ++(*begin);
             return;
@@ -100,8 +132,11 @@ static void do_backslash(void)
 // Does not include '\n' or '\r'
 errval_t shell_read_line(char** to, size_t* end_pos)
 {
+    bool inside_quote = false;
+    int escape_count = 0;
     size_t pos = 0;
     size_t buf_size = 16; // Init buffer size
+    bool* inside_quote_buf = malloc(buf_size);
     *to = malloc(buf_size);
 
     while (true)
@@ -110,12 +145,22 @@ errval_t shell_read_line(char** to, size_t* end_pos)
 		aos_rpc_serial_getchar(get_init_rpc(), &ret_char);
         if (ret_char == 0)
             continue;
+        if (ret_char == '\r')
+            ret_char = '\n';
+
 		if (ret_char == 127)
         {
-            if (pos)
+            if (pos && (*to)[pos-1] != '\n')
             {
                 --pos;
                 do_backslash();
+                --escape_count;
+                if (escape_count < 0)
+                    escape_count = 0;
+                if (pos > 0)
+                    inside_quote = inside_quote_buf[pos-1];
+                else
+                    inside_quote = false;
             }
             else
                 aos_rpc_serial_putchar(get_init_rpc(), '\a');
@@ -125,16 +170,39 @@ errval_t shell_read_line(char** to, size_t* end_pos)
         {
             buf_size *= 2;
             *to = realloc(*to, buf_size);
+            inside_quote_buf = realloc(inside_quote_buf, buf_size);
         }
-        if (ret_char == '\r' || ret_char == '\n')
+        if (ret_char == '"')
+        {
+            if (inside_quote)
+            {
+                if ((escape_count % 2) == 0)
+                    inside_quote = false;
+            }
+            else
+                inside_quote = true;
+        }
+        if (!inside_quote && ret_char == '\n')
         {
             (*to)[pos] = 0;
             *end_pos = pos;
     		aos_rpc_serial_putchar(get_init_rpc(), '\n');
+            free(inside_quote_buf);
             return SYS_ERR_OK;
         }
+        if (ret_char == '\\')
+            ++escape_count;
+        else
+            escape_count = 0;
+
         (*to)[pos] = ret_char;
+        inside_quote_buf[pos] = inside_quote;
         ++pos;
 		aos_rpc_serial_putchar(get_init_rpc(), ret_char);
+        if (ret_char == '\n' && inside_quote)
+        {
+            aos_rpc_serial_putchar(get_init_rpc(), ' ');
+            aos_rpc_serial_putchar(get_init_rpc(), '>');
+        }
 	}
 }
